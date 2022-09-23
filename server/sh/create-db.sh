4c465dec69db0c400eed22834081a85dcaa6f50d
andrew cooke <andrew@acooke.org>
2022-09-23 15:59:12 -0300

    seem to be using public schema

--- server/sh/create-db.sh
+++ server/sh/create-db.sh
@@ -4,4 +4,3 @@ cd "${BASH_SOURCE%/*}/" || exit 1
 ./assert-env.sh || exit 2
 PGPASSWORD=$PGPASSWD psql -U postgres -h localhost -c "create user bici with password '$BCPASSWD'"
 PGPASSWORD=$PGPASSWD psql -U postgres -h localhost -c "create database bici with owner bici"
-PGPASSWORD=$PGPASSWD psql -U postgres -h localhost bici -c "create schema bici"
